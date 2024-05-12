1e0b2bf066d4c47936caf28ad6bad045758bf327
anothervictoria <victoria.panidi@gmail.com>
2024-05-12 17:11:31 +0400

    Optional Chaining (?.)

--- starter/script.js
+++ starter/script.js
@@ -1,35 +1,35 @@
 'use strict';
 const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-const openingHours = {
-  [weekDays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekDays[3]]: {
-    open: 11,
-    close: 23,
-  },
-  [`day-${2 + 4}`]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
-
 // const openingHours = {
-//   thu: {
+//   [weekDays[3]]: {
 //     open: 12,
 //     close: 22,
 //   },
-//   fri: {
+//   [weekDays[3]]: {
 //     open: 11,
 //     close: 23,
 //   },
-//   sat: {
+//   [`day-${2 + 4}`]: {
 //     open: 0, // Open 24 hours
 //     close: 24,
 //   },
 // };
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 // Data needed for a later exercise
 const flights =
@@ -594,3 +594,29 @@ for (const [i, el] of menu.entries()) {
 //////////////////////////////////////
 
 //  Enhanced Object Literals
+//Optional Chaining (?.)
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// without optional chaining
+// console.log(restaurant.openingHours.mon.open);
+
+// with optional chaining
+console.log(restaurant.openingHours.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? `closed`;
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.com' }];
+console.log(users[0]?.name ?? 'User array empty');
